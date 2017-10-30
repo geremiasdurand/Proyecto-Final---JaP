a8a15edde39ee58ead104edcf13b3e586a9b6cbb
Cristian Castro <cristi4n12@hotmail.com>
2017-10-30 11:00:23 -0300

    Cambios De Mierda

---
+++ Proyecto Final - JaP/Validadores/ValidadorEdad.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EjemploASPNETMVCValidaciones.Validadores
+{
+    public class ValidadorEdad : ValidationAttribute
+    {
+        private int edad;
+
+        public ValidadorEdad(int edad)
+        {
+            this.edad = edad;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime fecha = (DateTime)value;
+                var diferencia = DateTime.Now - fecha;
+
+                if ((diferencia.Days / 365) >= this.edad)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("El alumno debe tener al menos " + this.edad + " años.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
