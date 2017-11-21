c3d9cb4853d2db38416cc37fced431ed59abf2a1
Geremias Durand <geremiasdurand@hotmail.com>
2017-11-21 04:22:37 -0300

    Avance un poco mas

--- Proyecto Final - JaP/Controllers/ClienteController.cs
+++ Proyecto Final - JaP/Controllers/ClienteController.cs
@@ -13,7 +13,7 @@ namespace Proyecto_Final___JaP.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
-            if (ValidarRol(Enumerados.Administrador))
+            if (ValidarRol(Enumerados.Administrador) || ValidarRol(Enumerados.Empleado))
             {
                 LogicaCliente logicaCliente = new LogicaCliente();
                 List<Cliente> cliente = logicaCliente.ListarTodos();
@@ -25,6 +25,81 @@ namespace Proyecto_Final___JaP.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (ValidarRol(Enumerados.Administrador) || ValidarRol(Enumerados.Empleado))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(Cliente cliente)
+        {
+            if (ValidarRol(Enumerados.Administrador) || ValidarRol(Enumerados.Empleado))
+            {
+                LogicaCliente logicaCliente = new LogicaCliente();
+                logicaCliente.Agregar(cliente);
+
+                return RedirectToAction("Index", "Cliente");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            if (ValidarRol(Enumerados.Administrador) || ValidarRol(Enumerados.Empleado))
+            {
+                LogicaCliente logicaCliente = new LogicaCliente();
+                Cliente cliente = logicaCliente.Buscar(id);
+
+                //NO PUEDO HACER FUNCAR ESTO
+
+                return View(cliente);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Cliente cliente)
+        {
+            if (ValidarRol(Enumerados.Administrador) || ValidarRol(Enumerados.Empleado))
+            {
+                LogicaCliente logicaCliente = new LogicaCliente();
+                logicaCliente.Modificar(cliente);
+
+                return RedirectToAction("Index", "Cliente");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            return View();
         }
 
         private bool ValidarRol(Enumerados Rol)
